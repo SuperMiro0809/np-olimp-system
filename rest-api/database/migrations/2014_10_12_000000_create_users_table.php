5ca7e31ee558ca40abc630fac360c87ecede75cf
Miroslav Balev <m.balev@editcreation.com>
2022-11-26 12:17:52 +0200

    setup laravel passport

--- rest-api/database/migrations/2014_10_12_000000_create_users_table.php
+++ rest-api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('type');
+            $table->integer('parent_id');
             $table->rememberToken();
             $table->timestamps();
         });
