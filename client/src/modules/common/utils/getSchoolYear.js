b670fe219b54591cb9dc34ca528bb768bf4902cd
Miroslav Balev <m.balev@editcreation.com>
2023-02-07 18:49:44 +0200

    add check if the date has passed 15th of September

--- client/src/modules/common/utils/getSchoolYear.js
+++ client/src/modules/common/utils/getSchoolYear.js
@@ -1,10 +1,15 @@
-export default function () {
+export default function getSchoolYear() {
     const date = new Date();
 
+    const currentDay = date.getDate();
     const monthIndex = date.getMonth();
     const currentYear = date.getFullYear();
 
     if (monthIndex >= 8) { //8 = September
+        if(monthIndex === 8 && currentDay < 15) {
+            return `${currentYear - 1} - ${currentYear}`;
+        }
+
         return `${currentYear} - ${currentYear + 1}`;
     }
 
