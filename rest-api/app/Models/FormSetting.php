251f012e54374b960b8e7d5215087d94b8f3cbb0
Miroslav Balev <m.balev@editcreation.com>
2023-04-15 13:59:45 +0300

    form settings table migration and model

---
+++ rest-api/app/Models/FormSetting.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FormSetting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'edit',
+        'delete',
+        'approved',
+        'submitted',
+        'form_id'
+    ];
+}
---
+++ rest-api/database/migrations/2023_04_15_105421_create_form_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('form_settings', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('edit')->default(true);
+            $table->boolean('delete')->default(true);
+            $table->boolean('approved')->default(false);
+            $table->boolean('submitted')->default(false);
+            $table->foreignId('form_id')->constrained('forms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('form_settings');
+    }
+};
